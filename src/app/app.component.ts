96639a8c3f9752e547fd7af4d1359eabcb0b38ff
John Piesco <jp@Johns-iMac.local>
2018-01-29 17:40:33 -0500

    functionality updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  myitems = [{item:'hi'},{item: 'hello'},{item: 'hey there'}];
+  selected = 'Select...'
+  myclick(sender){
+    console.log(sender);
+    this.selected = this.myitems[sender].item + sender;
+  }
 }
